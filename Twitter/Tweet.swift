2ab360f569aebbb6241cec8eed913eb4c51df33b
Xiaofei Long <dreloong@gmail.com>
2016-02-18 21:31:42 -0600

    Enable retweeting and favoriting

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -10,21 +10,66 @@ import Foundation
 
 class Tweet {
 
+    var id: String
     var createdAt: NSDate?
     var text: String?
     var user: User?
+    var favorited: Bool
+    var favoriteCount: Int
+    var retweeted: Bool
+    var retweetCount: Int
 
     init(dictionary: NSDictionary) {
+        id = dictionary["id_str"] as! String
+
         text = dictionary["text"] as? String
         user = User(dictionary: dictionary["user"] as! NSDictionary)
 
         let formatter = NSDateFormatter()
         formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
         createdAt = formatter.dateFromString(dictionary["created_at"] as! String)
+
+        favorited = dictionary["favorited"] as! Int != 0
+        retweeted = dictionary["retweeted"] as! Int != 0
+
+        favoriteCount = dictionary["favorite_count"] as! Int
+        retweetCount = dictionary["retweet_count"] as! Int
     }
 
     class func tweets(dictionaries: [NSDictionary]) -> [Tweet] {
         return dictionaries.map({ dictionary in Tweet(dictionary: dictionary) })
+    }
+
+    func favorite(completion: (tweet: Tweet?, error: NSError?) -> ()) {
+        TwitterClient.sharedInstance.POST(
+            "1.1/favorites/create.json?id=\(id)",
+            parameters: nil,
+            progress: nil,
+            success: { (_: NSURLSessionDataTask, _: AnyObject?) -> Void in
+                self.favorited = true
+                self.favoriteCount++
+                completion(tweet: self, error: nil)
+            },
+            failure: { (_: NSURLSessionDataTask?, error: NSError) -> Void in
+                completion(tweet: nil, error: error)
+            }
+        )
+    }
+
+    func retweet(completion: (tweet: Tweet?, error: NSError?) -> ()) {
+        TwitterClient.sharedInstance.POST(
+            "1.1/statuses/retweet/\(id).json",
+            parameters: nil,
+            progress: nil,
+            success: { (_: NSURLSessionDataTask, _: AnyObject?) -> Void in
+                self.retweeted = true
+                self.retweetCount++
+                completion(tweet: self, error: nil)
+            },
+            failure: { (_: NSURLSessionDataTask?, error: NSError) -> Void in
+                completion(tweet: nil, error: error)
+            }
+        )
     }
 
 }
--- Twitter/TweetTableViewCell.swift
+++ Twitter/TweetTableViewCell.swift
@@ -16,6 +16,10 @@ class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var screenNameLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var favoriteCountLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var retweetCountLabel: UILabel!
 
     var tweet: Tweet! {
         didSet {
@@ -26,6 +30,25 @@ class TweetTableViewCell: UITableViewCell {
             tweetTextLabel.text = tweet.text
             tweetTextLabel.sizeToFit()
             timeLabel.text = tweet.createdAt?.timeAgo()
+
+            favoriteCountLabel.text = tweet.favoriteCount != 0 ? String(tweet.favoriteCount) : ""
+            retweetCountLabel.text = tweet.retweetCount != 0 ? String(tweet.retweetCount) : ""
+
+            if tweet.favorited {
+                favoriteButton.setImage(UIImage(named: "Like-Active"), forState: .Normal)
+                favoriteCountLabel.textColor = UIColor(red:0.91, green:0.11, blue:0.31, alpha:1.0)
+            } else {
+                favoriteButton.setImage(UIImage(named: "Like-Default"), forState: .Normal)
+                favoriteCountLabel.textColor = UIColor(red:0.67, green:0.72, blue:0.76, alpha:1.0)
+            }
+
+            if tweet.retweeted {
+                retweetButton.setImage(UIImage(named: "Retweet-Active"), forState: .Normal)
+                retweetCountLabel.textColor = UIColor(red:0.1, green:0.81, blue:0.53, alpha:1.0)
+            } else {
+                retweetButton.setImage(UIImage(named: "Retweet-Default"), forState: .Normal)
+                retweetCountLabel.textColor = UIColor(red:0.67, green:0.72, blue:0.76, alpha:1.0)
+            }
         }
     }
 
@@ -35,6 +58,22 @@ class TweetTableViewCell: UITableViewCell {
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+
+    @IBAction func onFavoriteButtonTouchUp(sender: AnyObject) {
+        tweet.favorite({ (tweet: Tweet?, error: NSError?) in
+            if let tweet = tweet {
+                self.tweet = tweet
+            }
+        })
+    }
+
+    @IBAction func onRetweetButtonTouchUp(sender: AnyObject) {
+        tweet.retweet({ (tweet: Tweet?, error: NSError?) in
+            if let tweet = tweet {
+                self.tweet = tweet
+            }
+        })
     }
 
 }
