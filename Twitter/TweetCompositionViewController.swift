fd286854f2a2e5de098241bdc6a26532e027c6dc
Xiaofei Long <dreloong@gmail.com>
2016-02-24 01:39:39 -0600

    Track number of characters for tweet composition

--- Twitter/TweetCompositionViewController.swift
+++ Twitter/TweetCompositionViewController.swift
@@ -14,7 +14,12 @@ class TweetCompositionViewController: UIViewController {
     @IBOutlet weak var tweetTextView: UITextView!
 
     var toolbar: UIToolbar!
-    var tweetBarButtonItem: UIBarButtonItem!
+    var countLabel: UILabel!
+    var tweetButton: UIButton!
+
+    let twitterBlueColor = UIColor(red:0.33, green:0.67, blue:0.93, alpha:1.0)
+    let twitterRedColor = UIColor(red:0.91, green:0.11, blue:0.31, alpha:1.0)
+    let twitterGrayColor = UIColor(red:0.67, green:0.72, blue:0.76, alpha:1.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +28,9 @@ class TweetCompositionViewController: UIViewController {
         profileImageView.layer.cornerRadius = 4
         profileImageView.clipsToBounds = true
 
-        let spaceBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .FlexibleSpace,
-            target: nil,
-            action: nil
-        )
-        tweetBarButtonItem = UIBarButtonItem(
-            title: "Tweet",
-            style:  .Plain,
-            target: self,
-            action: "onTweet:"
-        )
-        toolbar = UIToolbar()
-        toolbar.items = [spaceBarButtonItem, tweetBarButtonItem, spaceBarButtonItem]
-        toolbar.sizeToFit()
+        initToolbar()
 
+        tweetTextView.delegate = self
         tweetTextView.inputAccessoryView = toolbar
         tweetTextView.becomeFirstResponder()
     }
@@ -53,7 +46,7 @@ class TweetCompositionViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
-    func onTweet(sender: AnyObject) {
+    func onTweetButtonTouchUp(sender: AnyObject) {
         TwitterClient.sharedInstance.createTweetWithParams(
             ["status": tweetTextView.text],
             completion: { (tweet: Tweet?, error: NSError?) in
@@ -63,6 +56,75 @@ class TweetCompositionViewController: UIViewController {
                 }
             }
         )
+    }
+
+    // MARK: - Helpers
+
+    func initToolbar() {
+        let spaceBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .FlexibleSpace,
+            target: nil,
+            action: nil
+        )
+
+        countLabel = UILabel()
+        countLabel.frame = CGRectMake(0, 0, 40, 30)
+        countLabel.text = "140"
+        countLabel.textColor = twitterGrayColor
+        countLabel.textAlignment = .Right
+        countLabel.font = UIFont.systemFontOfSize(13)
+
+        tweetButton = UIButton(type: .Custom)
+        tweetButton.frame = CGRectMake(0, 0, 70, 30)
+        tweetButton.layer.cornerRadius = 6
+        tweetButton.layer.borderColor = twitterGrayColor.CGColor
+        tweetButton.setTitle("Tweet", forState: .Normal)
+        tweetButton.titleLabel?.font = UIFont.boldSystemFontOfSize(13)
+        tweetButton.titleLabel?.textAlignment = .Center
+        tweetButton.addTarget(
+            self,
+            action: "onTweetButtonTouchUp:",
+            forControlEvents: .TouchUpInside
+        )
+
+        tweetButton.layer.borderWidth = 0.5
+        tweetButton.backgroundColor = UIColor.whiteColor()
+        tweetButton.setTitleColor(twitterGrayColor, forState: .Normal)
+        tweetButton.enabled = false
+
+        toolbar = UIToolbar()
+        toolbar.barTintColor = UIColor.whiteColor()
+        toolbar.layer.borderColor = twitterGrayColor.CGColor
+        toolbar.layer.borderWidth = 1
+        toolbar.items = [
+            spaceBarButtonItem,
+            UIBarButtonItem(customView: countLabel),
+            UIBarButtonItem(customView: tweetButton),
+        ]
+        toolbar.sizeToFit()
+    }
+
+}
+
+extension TweetCompositionViewController: UITextViewDelegate {
+
+    func textViewDidChange(textView: UITextView) {
+        let count = 140 - textView.text.characters.count
+        countLabel.text = "\(count)"
+
+        if count < 0 || count == 140 {
+            tweetButton.layer.borderWidth = 0.5
+            tweetButton.backgroundColor = UIColor.whiteColor()
+            tweetButton.setTitleColor(twitterGrayColor, forState: .Normal)
+            tweetButton.enabled = false
+        } else {
+            tweetButton.layer.borderWidth = 0
+            tweetButton.backgroundColor = twitterBlueColor
+            tweetButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            tweetButton.enabled = true
+        }
+
+        countLabel.textColor = count < 20 ? twitterRedColor : twitterGrayColor
     }
 
 }
